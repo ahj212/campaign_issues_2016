c2d4ca7b83e16d6c4bf22082fe7b9add2f31e0d9
Hwan Ho Jeoung <ahj212@gmail.com>
2015-08-27 17:04:50 -0400

    finished setting up candidates controller

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,4 @@
 class CandidatesController < ApplicationController
-
   def index
 
   end
@@ -9,7 +8,6 @@ class CandidatesController < ApplicationController
   	@issue = Issue.all
   	#gets all the rows in candidate_issues where candidate_id corresponds to params id
   	@candidate_issue = CandidateIssue.where(:candidate_id => params[:id])
-  	# binding.pry
   end
 
   def compare  	
@@ -20,43 +18,4 @@ class CandidatesController < ApplicationController
   	@candidate_one_issues = @candidate_one.candidate_issues.where(:issue_id => params[:issue_id])
   	@candidate_two_issues = @candidate_two.candidate_issues.where(:issue_id => params[:issue_id])
   end
-  # def index
-  #   @artists = Artist.all
-  # end
-
-  # def show
-  #   @artist = Artist.find(params[:id])
-  # end
-
-  # def new
-  #   @artist = Artist.new
-  # end
-
-  # def create
-  #   @artist = Artist.create(artist_params)
-  #   redirect_to artist_path(@artist)
-  # end
-
-  # def edit
-  #   @artist = Artist.find(params[:id])
-  # end
-
-  # def update
-  #   @artist = Artist.find(params[:id])
-  #   @artist.update(artist_params)
-  #   redirect_to artist_path(@artist)
-  # end
-
-  # def destroy
-  #   @artist = Artist.find(params[:id])
-  #   @artist.destroy
-  #   redirect_to artists_path
-  # end
-
-  # private
-
-  # def artist_params
-  #   params.require(:aritist).permit(:name, :img_url, :nationality, :genre)
-  # end
-
 end
