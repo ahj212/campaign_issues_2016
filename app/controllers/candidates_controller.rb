fda4f67e3d6ac9fd6bf229ba9739aac94044ce3e
Hwan Ho Jeoung <ahj212@gmail.com>
2015-08-26 20:05:30 -0400

    started candidates controller

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,59 @@
+class CandidatesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def compare  	
+  	@candidate_one = Candidate.find(params[:candidate_id])
+  	@candidate_two = Candidate.find(params[:second_candidate_id])
+  	@issue = Issue.find(params[:issue_id])
+
+  	@candidate_one_issues = @candidate_one.candidate_issues.where(:issue_id => params[:issue_id])
+  	@candidate_two_issues = @candidate_two.candidate_issues.where(:issue_id => params[:issue_id])
+  	binding.pry
+  end
+  # def index
+  #   @artists = Artist.all
+  # end
+
+  # def show
+  #   @artist = Artist.find(params[:id])
+  # end
+
+  # def new
+  #   @artist = Artist.new
+  # end
+
+  # def create
+  #   @artist = Artist.create(artist_params)
+  #   redirect_to artist_path(@artist)
+  # end
+
+  # def edit
+  #   @artist = Artist.find(params[:id])
+  # end
+
+  # def update
+  #   @artist = Artist.find(params[:id])
+  #   @artist.update(artist_params)
+  #   redirect_to artist_path(@artist)
+  # end
+
+  # def destroy
+  #   @artist = Artist.find(params[:id])
+  #   @artist.destroy
+  #   redirect_to artists_path
+  # end
+
+  # private
+
+  # def artist_params
+  #   params.require(:aritist).permit(:name, :img_url, :nationality, :genre)
+  # end
+
+end
