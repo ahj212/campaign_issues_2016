63b49d6560106aefd67d19811cde651ddde45595
Hwan Ho Jeoung <ahj212@gmail.com>
2015-08-27 17:03:37 -0400

    setting up issues controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,12 +1,11 @@
 class IssuesController < ApplicationController
-
   def index
 
   end
 
   def show
-
+    @issue = Issue.all
+    @candidate_issue = CandidateIssue.where(:issue_id => params[:id])
+    # binding.pry
   end
-
-
 end
